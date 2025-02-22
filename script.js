2b56f04dd351a0c7f215b3752e54567edf0aa249
dragonvedb <dragonvedb@gmail.com>
2025-02-22 12:57:24 +0300

    Set pan and zoom limits

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const board = document.querySelector('main')
 console.log(board)
 
-panzoom(board)
+panzoom(board, {
+    bounds: true, 
+    boundsPadding: 0.5,
+    maxZoom: 3,
+    minZoom: 0.4
+}) 
