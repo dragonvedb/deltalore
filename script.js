13d1bb20a80cd77febda9c5795f2c22c99f124e7
dragonvedb <dragonvedb@gmail.com>
2025-02-23 00:32:52 +0300

    Style nodes

--- script.js
+++ script.js
@@ -27,8 +27,9 @@ const converter = new showdown.Converter()
 function createNode(data) {
     let node = document.createElement('div')
     node.setAttribute('id', data.id)
+    node.setAttribute('class', 'node')
     if (data.type = 'file') node.setAttribute('data-file', data.file)
-    node.style.cssText = `position: absolute; left: ${data.x + 5000}px; top: ${data.y + 5300}px; width: ${data.width}px; height: ${data.height}px; background-color: rgba(120, 120, 30, 0.3)`
+    node.style.cssText = `left: ${data.x + 5000}px; top: ${data.y + 5300}px; width: ${data.width}px; height: ${data.height}px;`
     node.textContent = data[data.type]
     node.addEventListener('click', e => loadFile(e.target.getAttribute('data-file')))
     board.appendChild(node)
